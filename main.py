fc6f3edf21eebc89dd9a6fbc9e0d104dea0e429b
Roman A. <aksenov.nsk.r.a@gmail.com>
2024-09-13 17:50:32 +0300

    Start project

---
+++ main.py
@@ -0,0 +1,41 @@
+from decimal import Decimal
+from datetime import date, datetime
+
+
+def add(items: dict, title: str, amount: Decimal, expiration_date: str = None):
+    true_date = datetime.strptime(expiration_date, "%Y-%m-%d")
+    item = {'amount': amount, 'expiration_date': true_date.date()}
+    if title in items:
+        items[title].append(item)
+    else:
+        items[title] = [item]
+
+
+def add_by_note(items: dict, note: str):
+    split_note = note.split()
+    if split_note[2]:
+        add(items, split_note[0], Decimal(split_note[1]), split_note[2])
+    else:
+        add(items, split_note[0], Decimal(split_note[1]))
+
+
+def find(items: dict, needle):
+    ...
+
+
+def amount(items: dict, needle):
+    ...
+
+
+def expire(items: dict, in_advance_days=0):
+    ...
+
+
+# Test part
+goods = {
+    'Пельмени Универсальные': [
+        {'amount': Decimal('0.5'), 'expiration_date': date(2023, 9, 30)}
+    ]
+}
+add_by_note(goods, "Яйца 4 2023-07-15 ")
+print(goods)
